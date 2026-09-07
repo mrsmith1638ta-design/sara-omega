6860ed22a524e16d24471596c9f6ed0942c7b188
mrsmith1638ta-design <mrsmith1638t.a@gmail.com>
2026-09-07 19:09:43 -0400

    feat: export IoT adapters

---
+++ app/iot/adapters/__init__.py
@@ -0,0 +1,4 @@
+from .android import AndroidCompanionAdapter
+from .base import AdapterOutcome,AdapterResult,DeviceAdapter
+from .sony_ht_st5000 import SonyHTST5000Adapter
+__all__=['AndroidCompanionAdapter','AdapterOutcome','AdapterResult','DeviceAdapter','SonyHTST5000Adapter']
